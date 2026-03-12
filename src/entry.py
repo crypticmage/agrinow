9a7fffa5ec1e4987306a7bfea6ff61c30b1edacb
Rakshit Rangarajan <61931725+Rakshit-Rangarajan@users.noreply.github.com>
2026-03-13 00:46:02 +0530

    Fix CORS middleware configuration

--- src/entry.py
+++ src/entry.py
@@ -43,6 +43,7 @@ app = FastAPI(
 # Configure CORS Middleware
 app.add_middleware(
     CORSMiddleware,
+    low_origins=['*']
     allow_origin_regex=".*",  
     allow_credentials=True,
     allow_methods=["*"],  
